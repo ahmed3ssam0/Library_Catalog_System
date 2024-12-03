cc54d51d8a7814c94658f7b738e9ddbc84fb3911
ahmed3ssam0 <ahmedessam1427@hotmail.com>
2024-12-03 23:52:17 +0200

    Classes Update

--- src/main/java/com/example/library_catalog_system/Book.java
+++ src/main/java/com/example/library_catalog_system/Book.java
@@ -1,20 +1,33 @@
 package com.example.library_catalog_system;
 
 public class Book {
-    private static int BookId = 0;
-    private int price, publication_year;
+    private static int BookId = 0, numOfCopies = 0;
+    private int price;
+    private int publication_year;
+    private final int numOfPages;
     private String title;
-    private boolean status;
     protected Author author;
-    public Book(String title, int price, int publication_year, boolean status, Author author) {
+    boolean found = false;
+    public Book(String title, int numOfPages, int price, int publication_year, Author author) {
         this.title = title;
         this.price = price;
         this.publication_year = publication_year;
-        this.status = status;
+        this.numOfPages = numOfPages;
         this.author = author;
-        ++BookId;
+        for (int i = 0; i < Library.getBooks().size(); i++) {
+            if (Library.getBooks().get(i).getTitle().equals(title) && Library.getBooks().get(i).getAuthor().getName().equals(author.getName())) {
+                found = true;
+                numOfCopies++;
+            }
+        }
+        if (!found) {
+            ++BookId;
+        }
     }
 
+    public int getBookId() {
+        return BookId;
+    }
     public String getTitle() {
         return title;
     }
@@ -33,17 +46,27 @@ public class Book {
     public void setPublication_year(int publication_year) {
         this.publication_year = publication_year;
     }
-    public boolean isStatus() {
-        return status;
-    }
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
     public Author getAuthor() {
         return author;
     }
     public void setAuthor(Author author) {
         this.author = author;
+    }
+
+    public void display_book_info() {
+        System.out.println("Display Book : " + getTitle() + " Information");
+        System.out.println(" ================================================================");
+        System.out.println("Book ID: " + getBookId());
+        System.out.println("Title: " + getTitle());
+        System.out.println("Number of Pages: " + numOfPages);
+        System.out.println("Publication Year: " + getPublication_year());
+        System.out.println("Price: " + getPrice());
+        System.out.println("Author: " + getAuthor().getName());
+        if (numOfCopies > 0) {
+            System.out.println("Status: Available - " + numOfCopies + " copies");
+        } else {
+            System.out.println("Status: Not available - " + numOfCopies + " copies");
+        }
     }
 
 }
--- src/main/java/com/example/library_catalog_system/Library.java
+++ src/main/java/com/example/library_catalog_system/Library.java
@@ -1,6 +1,33 @@
 package com.example.library_catalog_system;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    String name, address;
-    static int numOfSections, numOfBooks;
+    private String name, address;
+    private static int numOfSections, numOfBooks;
+    private static List<Book> books;
+    public Library(String name, String address) {
+        this.name = name;
+        this.address = address;
+        books = new ArrayList<>();
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public static List<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(Book book) {
+        books.add(book);
+    }
 }
