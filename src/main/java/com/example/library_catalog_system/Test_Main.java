66b7486ab13e6e409fda860a1e2c1bb9350330b6
Menna Ezzat <mennaezzat130905@gmail.com>
2024-12-12 19:05:59 +0200

    Add files via upload

---
+++ src/main/java/com/example/library_catalog_system/Test_Main.java
@@ -0,0 +1,27 @@
+package com.example.library_catalog_system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
+
+        stage.setScene(scene);
+        stage.setTitle("Library Catalog System");
+        stage.setWidth(stage.getWidth());
+        stage.setHeight(stage.getHeight());
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
